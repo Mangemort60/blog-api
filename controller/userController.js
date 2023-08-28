f7c92b02688b745c1dece9bbe7db3922b59cfb7b
Hafid HADDAOUI <hahaddaoui@gmail.com>
2023-08-28 12:33:17 +0200

    changed checking logic for deleting user by user or admin

--- controller/userController.js
+++ controller/userController.js
@@ -20,21 +20,22 @@ const userController = {
       const userIdToDelete = req.params.id;
       const userId = req.params.author;
       const user = await User.findById(userIdToDelete);
+      const userDeleting = await User.findById(userId);
       if (!user) {
         return res
           .status(404)
           .json({ message: "l'utilisateur n'a pas été trouvé" });
       }
-      if (user._id.toString() !== userId || !user.isAdmin) {
-        return res.status(403).json({
-          message:
-            "vous n'avez pas l'autorisation pour supprimer cet utilisateur",
+      if (user._id.toString() === userId || userDeleting.isAdmin) {
+        const deletedUser = await User.findByIdAndDelete(userIdToDelete);
+        return res.status(200).json({
+          message: "l'utilisateur a bien été supprimé",
+          data: deletedUser,
         });
       }
-      const deletedUser = await User.findByIdAndDelete(userIdToDelete);
-      res.status(200).json({
-        message: "l'utilisateur a bien été supprimé",
-        data: deletedUser,
+      return res.status(403).json({
+        message:
+          "vous n'avez pas l'autorisation pour supprimer cet utilisateur",
       });
     } catch (error) {
       res.status(500).json({
