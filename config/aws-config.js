82bd4fda47d428bfdd4823f0742604ca320323a7
Hafid HADDAOUI <hahaddaoui@gmail.com>
2023-10-11 19:48:44 +0200

    edited aws config

--- config/aws-config.js
+++ config/aws-config.js
@@ -1,7 +1,8 @@
 const { S3 } = require('@aws-sdk/client-s3');
+const { fromEnv } = require('@aws-sdk/credential-provider-env');
 
 const region = 'eu-west-3'; // Vous pouvez spécifier la région si elle n'est pas déjà dans votre fichier de config AWS
 
-const s3 = new S3({ region });
+const s3 = new S3({ region, credentials: fromEnv() });
 
 module.exports = s3;
